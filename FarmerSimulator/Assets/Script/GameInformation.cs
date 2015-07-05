f3f84b41169288625fba2337c8a74ae6bd0afaff
Ares Lee <fireares@hotmail.com>
2015-07-05 08:39:51 -0700

    added the timer and time progress bar to show the time in game

--- FarmerSimulator/Assets/Script/GameInformation.cs
+++ FarmerSimulator/Assets/Script/GameInformation.cs
@@ -1,23 +1,54 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UIWidgets;
+using UnityEngine.UI;
 
 public class GameInformation : MonoBehaviour {
 	float timer;
-	float timeIncreasingSpeed;
+	float timeIncreasingSpeed=5;
 	public bool isTimeRunning;
+	int daysCounter=0;
+	float percentageOfProgressBar;
+	Text textOnProgressBar;
+	Text textBelowProgressBar;
 
+
+	private Progressbar timeProgressbar;
 
 	// Use this for initialization
 	void Start () {
-	
+		timeProgressbar = GameObject.Find ("TimeProgressbar").GetComponent<Progressbar> ();
+		timeProgressbar.Max = 100;
+		textOnProgressBar = GameObject.FindGameObjectWithTag ("TextOnProgressBar").GetComponent<Text> ();
+		textBelowProgressBar=GameObject.FindGameObjectWithTag ("TextbelowProgressBar").GetComponent<Text> ();
+	//	timeProgressbar.Value = 10;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		timerFunc ();
+		processBarForTimeFunc ();
 
+		Debug.Log("timer: "+(int)timer+ " day: "+daysCounter+" percentage: "+percentageOfProgressBar);
+	}
+
+	void timerFunc(){
 		if (isTimeRunning) {
-			timer += timeIncreasingSpeed;
+			//1 second in the real life = 1 hour in the game by defult
+			timer += timeIncreasingSpeed*Time.deltaTime;
 		}
+		//when reach every 24 hours in the game world, dayCounter will be added one
+		daysCounter = (int)(timer / 24);
 
+		percentageOfProgressBar = (timer % 24) / 24;
+	}
+
+	void processBarForTimeFunc(){
+
+		timeProgressbar.Value = (int)(percentageOfProgressBar*100);
+		int currentHour = (int)(timer % 24);
+
+		textOnProgressBar.text="Time: "+currentHour+":00";
+		textBelowProgressBar.text = "Day "+daysCounter;
 	}
 }
